96aa335cd0f33200857e53e272042eb2b1782b26
Jared Hanson <jaredhanson@gmail.com>
2011-03-06 16:02:18 -0800

    First commit.

---
+++ examples/client.js
@@ -0,0 +1,27 @@
+require.paths.unshift('../lib')
+var xmpp = require('node-xmpp');
+var inflect = require('inflect');
+
+var options = {
+  type: 'client',
+  jid: 'user@example.com',
+  password: 'secret',
+  host: 'example.com',
+  port: 5222
+};
+
+var connection = inflect.createConnection(options);
+connection.use(inflect.logger);
+connection.use(inflect.serviceDiscovery, [ { category: 'client', type: 'bot' } ],
+                                         ['http://jabber.org/protocol/disco#info'] );
+connection.use(inflect.serviceUnavailable);
+connection.use(inflect.errorHandler);
+
+connection.on('online', function() {
+  console.log('ONLINE!');
+  connection.send(new xmpp.Element('presence'));
+});
+
+connection.on('error', function(err) {
+  console.log('ERROR: ' + err);
+});
