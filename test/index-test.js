4bf3a7f0628330a1f3379f77360021384f073d2c
Jared Hanson <jaredhanson@gmail.com>
2012-05-10 21:49:41 -0700

    Test cases for module.

--- test/index-test.js
+++ test/index-test.js
@@ -4,19 +4,42 @@ var junction = require('junction');
 var util = require('util');
 
 
-vows.describe('Library').addBatch({
+vows.describe('junction').addBatch({
   
-  /*
-  'junction': {
-    topic: function() {
-      return null;
-    },
-    
-    'should report a version': function (x) {
-      assert.isString(junction.version);
-    },
+  'should export version': function() {
+    assert.isString(junction.version);
   },
   
+  'should export create function': function() {
+    assert.isFunction(junction);
+    assert.isFunction(junction.create);
+    assert.equal(junction, junction.create);
+  },
+  
+  'should export constructors': function() {
+    assert.isFunction(junction.JID);
+    assert.isFunction(junction.XMLElement);
+  },
+  
+  'should export elements': function() {
+    assert.isFunction(junction.elements.Element);
+    assert.isFunction(junction.elements.IQ);
+    assert.isFunction(junction.elements.Message);
+  },
+  
+  'should export middleware': function() {
+    assert.isObject(junction.middleware);
+  },
+  
+  'should export filters': function() {
+    assert.isObject(junction.filters);
+  },
+  
+  'should export errors': function() {
+    assert.isFunction(junction.StanzaError);
+  },
+  
+  /*
   'create connection with client type': {
     topic: function() {
       return new junction.createConnection({ type: 'client', jid: 'user@invalid.host', disableStream: true });
